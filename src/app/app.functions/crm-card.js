c4b959823b01abf83de0a513f793a7b108e9f625
Dennis Edson <dedson@hubspot.com>
2022-08-12 14:28:32 -0400

    sure

--- src/app/app.functions/crm-card.js
+++ src/app/app.functions/crm-card.js
@@ -13,11 +13,7 @@ exports.main = async (context = {}, sendResponse) => {
   };
 
   try {
-    const { data } = await axios.get("https://app.asana.com/api/1.0/users/me?opt_pretty=true&opt_fields=followers,assignee",{
-      headers:{
-        'authorization': `Bearer 1/${process.env.asana_pat}`
-      }
-    });
+    const { data } = await axios.get("https://app.asana.com/api/1.0/users/me?opt_pretty=true&opt_fields=followers,assignee");
 
     const quoteSections = [
       {
